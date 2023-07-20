2693a168e80cc14fb073ff47086cee3d3370e9a3
RSeto14 <107923048+RSeto14@users.noreply.github.com>
2023-07-20 23:40:55 +0900

    Update mediapipe_test.py

--- assignment/mediapipe_test.py
+++ assignment/mediapipe_test.py
@@ -4,31 +4,28 @@ import math
 
 def main():
 
-    ## initialize pose estimator
+    ## initialize pose estimator ###################################################
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
     pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1600)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 900)
+    ###############################################################################
 
     
     while cap.isOpened():
         try:
-            # read frame
-            _, frame = cap.read()
-            # convert to RGB
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # process the frame for pose detection
-            pose_results = pose.process(frame_rgb)
-            # draw skeleton on the frame
-            mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-            #Flip horizontal
-            frame = cv2.flip(frame, 1)
+            ## mediapipe #######################################################################################
+            _, frame = cap.read() # read frame
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert to RGB
+            pose_results = pose.process(frame_rgb) # process the frame for pose detection
+            mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS) # draw skeleton on the frame
+            frame = cv2.flip(frame, 1) # Flip horizontal
+            ####################################################################################################
             
-
+            ## Calculate joint angle #################################################################################
             "------ change here -------------------------------------------------------------------------------------"
-
             if pose_results.pose_landmarks != None:
                 RShoulder = (pose_results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].x, pose_results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y)
                 #https://developers.google.com/mediapipe/solutions/vision/pose_landmarker/python
@@ -38,7 +35,9 @@ def main():
 
             print(RShoulder)
             "--------------------------------------------------------------------------------------------------------"
+            ##########################################################################################################
 
+            ## openCV #####################################################################################
             #text
             cv2.putText(frame,"enter text here",(10,60), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,0),3,cv2.LINE_AA)
             
@@ -46,6 +45,7 @@ def main():
             cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
             cv2.resizeWindow('Output', 900, 500)
             cv2.imshow('Output', frame)
+            ###############################################################################################
             
         except:
             break
